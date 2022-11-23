aa10db7af7546b2f18302fe9c5ef4d6f8e0e2899
Scoripo <aradusewicz@gmail.com>
2022-11-22 21:35:34 -0700

    added code to index js file

--- index.js
+++ index.js
@@ -3,8 +3,54 @@ const inquirer = require("inquirer");
 const managerFile = require("./directories/lib/manager");
 const engineerFile = require("./directories/lib/engineer");
 const internFile = require("./directories/lib/intern");
-
+const fs = require('fs');
 
 //initialize the questions:
-
+const { writeFile } = require('fs');
 //start the questions
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your employee ID?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?',
+        },
+        {
+            type: 'input',
+            name: 'Role',
+            message: 'What is your role? Please select one.',
+            choices: ['manager', 'engineer', 'intern'],
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: 'What is your office number? Please type none if you do not have an office number.',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub Username? Please type none if you do not have a GitHub username.',  
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What is the school you are currently attending? Please put none if you are not an intern.',
+        },
+    ])
+    .then((data) => {
+        const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+        fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) => 
+        err ? console.log(err) : console.log('Success')
+        );
+    });
+}
